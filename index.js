89e36852a0fa5d9d88e8108b079b8d30a9f570bb
fitse01 <fitsumtafese01@gmail.com>
2024-09-21 23:51:30 +0300

    server setup

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const express = require("express");
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4040 ;
+const app = express()
+app.use(express.json());
+app.post("*" ,async(req,res)=>{
+    res.send("hello post ");
+})
+app.get("*" , async(req,res)=>{
+    res.send("hello get ")
+})
+app.listen(PORT , function(err){
+    if(err) console.log(err);
+    console.log("Server Listening on Port " , PORT)
+})
